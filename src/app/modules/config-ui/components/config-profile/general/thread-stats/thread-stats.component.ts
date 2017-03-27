812a4dea2b3a999574377829b9515e6dd53c422d
akshay garg <gargakshay2010@gmail.com>
2017-03-27 16:22:50 +0530

    Added General Profile Component

---
+++ src/app/modules/config-ui/components/config-profile/general/thread-stats/thread-stats.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thread-stats',
+  templateUrl: './thread-stats.component.html',
+  styleUrls: ['./thread-stats.component.css']
+})
+export class ThreadStatsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
