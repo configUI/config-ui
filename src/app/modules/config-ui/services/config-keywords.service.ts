fbf2f3e23d86627a40fcec4d9a6d35f79130aa15
akshay garg <gargakshay2010@gmail.com>
2017-03-28 15:39:42 +0530

    Changes for Cleanup hotspot code and indentation

--- src/app/modules/config-ui/services/config-keywords.service.ts
+++ src/app/modules/config-ui/services/config-keywords.service.ts
@@ -2,36 +2,48 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 import { ConfigRestApiService } from './config-rest-api.service';
 import * as URL from '../constants/config-url-constant';
-import {ServiceEntryPoint, IntegrationPTDetection, ErrorDetection} from '../containers/instrumentation-data';
+import { ServiceEntryPoint, IntegrationPTDetection, ErrorDetection } from '../containers/instrumentation-data';
 
 @Injectable()
 export class ConfigKeywordsService {
 
-  keywordData: Object = {};
+  private _keywordData: Object;
+
+
+  public get keywordData(): Object {
+    if (!this._keywordData)
+      return {};
+    return this._keywordData;
+  }
+
+  public set keywordData(value: Object) {
+    this._keywordData = value;
+  }
+
 
   constructor(private _restApi: ConfigRestApiService) { }
 
   /** For Getting all keywordData data */
-  getProfileKeywords(profileId){
+  getProfileKeywords(profileId) {
     this._restApi.getDataByGetReq(`${URL.GET_KEYWORDS_DATA}/${profileId}`)
-    .subscribe(data => this.keywordData = data);
+      .subscribe(data => this.keywordData = data);
   }
 
   /** For save all keywordData data */
-  saveProfileKeywords(profileId){
+  saveProfileKeywords(profileId) {
     this._restApi.getDataByPostReq(`${URL.UPDATE_KEYWORDS_DATA}/${profileId}`, this.keywordData)
-    .subscribe(data => this.keywordData = data);
+      .subscribe(data => this.keywordData = data);
   }
 
   /**Service Entry Point */
-  getServiceEntryPointList(profileId): Observable<ServiceEntryPoint[]>{
+  getServiceEntryPointList(profileId): Observable<ServiceEntryPoint[]> {
     return this._restApi.getDataByGetReq(`${URL.FETCH_SERVICE_POINTS_TABLEDATA}/${profileId}`);
   }
 
-  addServiceEntryPointData(data): Observable<ServiceEntryPoint>{
+  addServiceEntryPointData(data): Observable<ServiceEntryPoint> {
     return this._restApi.getDataByPostReq(URL.ADD_NEW_SERVICE_ENTRY_POINTS, data);
   }
-  
+
   /**For Integration PT Detection */
   getIntegrationPTDetectionList(profileId): Observable<IntegrationPTDetection[]> {
     return this._restApi.getDataByGetReq(`${URL.FETCH_BACKEND_TABLEDATA}/${profileId}`);
@@ -54,7 +66,7 @@ export class ConfigKeywordsService {
     return this._restApi.getDataByPostReq(URL.ADD_NEW_ERROR_DETECTION, data);
   }
 
-  editErrorDetection(data): Observable<ErrorDetection>{
+  editErrorDetection(data): Observable<ErrorDetection> {
     let url = `${URL.ADD_NEW_ERROR_DETECTION}/${data.errDetectionId}`
     return this._restApi.getDataByPutReq(url, data);
   }
