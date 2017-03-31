c4acd543ce38abda0eb257930ae413c2d9f6845c
Satendra19 <satendra.vashishtha@gmail.com>
2017-03-31 19:45:13 +0530

    Changes for the Exception Capturing keyword

--- src/app/modules/config-ui/components/config-profile/general/exception/exception.component.ts
+++ src/app/modules/config-ui/components/config-profile/general/exception/exception.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { KeywordData } from '../../../../containers/keyword-data';
+import { ConfigKeywordsService } from '../../../../services/config-keywords.service';
+import { ExceptionData } from '../../../../containers/exception-capture-data';
 
 @Component({
   selector: 'app-exception',
@@ -7,9 +10,107 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExceptionComponent implements OnInit {
 
-  constructor() { }
+  /**This is to send data to parent component(General Screen Component) for save keyword data */
+  @Output()
+  keywordData = new EventEmitter();
+
+  /**These are those keyword which are used in current screen. */
+  keywordList: string[] = ['instrExceptions'];
+
+  selectedValue: string = 'unhandled';
+
+  exception: Object;
+
+  constructor(private configKeywordsService: ConfigKeywordsService) { }
+
+  exceptionData: ExceptionData;
+
+  exceptionForm: boolean = true;
 
   ngOnInit() {
+    this.getKeywordData();
   }
 
+
+  getKeywordData() {
+    let keywordData = this.configKeywordsService.keywordData;
+    this.exception = {};
+
+    this.keywordList.forEach((key) => {
+      if (keywordData.hasOwnProperty(key)) {
+        this.exception[key] = keywordData[key];
+      }
+    });
+
+    if ((this.exception["instrExceptions"].value).includes("%20")) {
+      let arr = (this.exception["instrExceptions"].value).split("%20")
+      this.exceptionData = new ExceptionData();
+
+      if(arr[0] === "1")
+        this.exceptionData.instrumentException = true;
+      else
+        this.exceptionData.instrumentException = false;
+
+      if(arr[1] == "1")
+        this.exceptionData.exceptionTrace = true;
+      else
+        this.exceptionData.exceptionTrace = false;
+        
+      this.exceptionData.exceptionType = arr[2];
+      
+      if(arr.length > 2)
+        this.exceptionData.exceptionTraceDepth = arr[3];
+    }
+    else
+    {
+         this.exceptionData = new ExceptionData();
+      if(this.exception["instrExceptions"].value == 0)
+      {
+      this.exceptionData.instrumentException = false;
+      this.exceptionData.exceptionTrace = false;
+      this.exceptionData.exceptionType = "unhandled"
+      }
+      else if(this.exception["instrExceptions"].value == 1)
+      {
+      this.exceptionData.instrumentException = true;
+      this.exceptionData.exceptionTrace = false;
+      this.exceptionData.exceptionType = "unhandled"
+      }
+
+    }
+  }
+
+  saveKeywordData(data) {
+    let instrValue = this.instrExceptionValue(data);
+    for(let key in this.exception){
+          if(key == 'instrExceptions')
+            this.exception[key]["value"] = instrValue;
+      }
+    this.keywordData.emit(this.exception);
+  }
+
+  // Method used to construct the value of instrException keyword.
+  instrExceptionValue(data) {
+    var instrVal = {};
+    if (data.form._value.instrumentException === "false" || data.form._value.instrumentException === false) {
+      instrVal = "0";
+    }
+    else {
+      instrVal = "1";
+
+      if (data.form._value.exceptionTrace === "true" || data.form._value.exceptionTrace === true)
+        instrVal = instrVal + "%201";
+      else
+        instrVal = instrVal + "%200";
+
+      if (data.form._value.exceptionType === 'unhandled')
+        instrVal = instrVal + "%200";
+      else
+        instrVal = instrVal + "%201"
+
+      if (data.form._value.exceptionTrace === "true" || data.form._value.exceptionTrace === true)
+        instrVal = instrVal + "%20" + data.form._value.exceptionTraceDepth;
+    }
+    return instrVal;
+  }
 }
