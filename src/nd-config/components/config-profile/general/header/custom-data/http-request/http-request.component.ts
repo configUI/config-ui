6c0cecd74869d98fd1f54d9c6f2d8bc4cb90b966
Lucky <luckygoyal725@gmail.com>
2017-04-14 13:41:30 +0530

    changes for Req/Res header

--- src/nd-config/components/config-profile/general/header/custom-data/http-request/http-request.component.ts
+++ src/nd-config/components/config-profile/general/header/custom-data/http-request/http-request.component.ts
@@ -1,6 +1,49 @@
+// import { Component, OnInit } from '@angular/core';
+// import { ConfigUiUtility } from '../../../../../../utils/config-utility';
+// import { SelectItem } from 'primeng/primeng';
+
+// @Component({
+//   selector: 'app-http-request',
+//   templateUrl: './http-request.component.html',
+//   styleUrls: ['./http-request.component.css']
+// })
+// export class HttpRequestComponent implements OnInit {
+
+//   specifiedHeaderType: SelectItem[];
+//   selectedHeaderType: string;
+
+//   headerTypes: SelectItem[];
+//   selectedHeader: string;
+
+//   captureMode: SelectItem[];
+//   selectedCaptureMode: string;
+
+//   constructor() {
+//     var reqHdrList = ['Accept-Charset', 'Accept-Datetime', 'Accept-Encoding', 'Accept-Language', 'Accept', 'Authorization',
+//       'Cache-Control', 'Connection', 'Content-Length', 'Content-MD5', 'Cookie', 'DNT', 'Date', 'Expect',
+//       'Front-End-Https', 'Host', 'If-Match', 'If-Modified-Since', 'If-None-Match', 'If-Range', 'Proxy-Connection',
+//       'Range', 'Referer', 'TE', 'Upgrade', 'User-Agent', 'Via', 'X-ATT-DeviceId', 'X-Forwarded-For', 'X-Forwarded-Proto',
+//       'X-Requested-With', 'X-Wap-Profile'];
+
+//     this.specifiedHeaderType = ConfigUiUtility.createDropdown(reqHdrList);
+
+//     var reqHdrType = ['ALL Headers' , 'Specified Headers'];
+
+//     this.headerTypes = ConfigUiUtility.createDropdown(reqHdrType);
+
+//     var reqcaptureMode = ['complete' , 'brief'];
+
+//     this.captureMode = ConfigUiUtility.createDropdown(reqcaptureMode);
+
+
+//   }
+
+//   ngOnInit() {
+//   }
+
+// }
+
 import { Component, OnInit } from '@angular/core';
-import { ConfigUiUtility } from '../../../../../../utils/config-utility';
-import { SelectItem } from 'primeng/primeng';
 
 @Component({
   selector: 'app-http-request',
@@ -9,36 +52,16 @@ import { SelectItem } from 'primeng/primeng';
 })
 export class HttpRequestComponent implements OnInit {
 
-  specifiedHeaderType: SelectItem[];
-  selectedHeaderType: string;
+  constructor() { }
 
-  headerTypes: SelectItem[];
-  selectedHeader: string;
-
-  captureMode: SelectItem[];
-  selectedCaptureMode: string;
-
-  constructor() {
-    var reqHdrList = ['Accept-Charset', 'Accept-Datetime', 'Accept-Encoding', 'Accept-Language', 'Accept', 'Authorization',
-      'Cache-Control', 'Connection', 'Content-Length', 'Content-MD5', 'Cookie', 'DNT', 'Date', 'Expect',
-      'Front-End-Https', 'Host', 'If-Match', 'If-Modified-Since', 'If-None-Match', 'If-Range', 'Proxy-Connection',
-      'Range', 'Referer', 'TE', 'Upgrade', 'User-Agent', 'Via', 'X-ATT-DeviceId', 'X-Forwarded-For', 'X-Forwarded-Proto',
-      'X-Requested-With', 'X-Wap-Profile'];
-
-    this.specifiedHeaderType = ConfigUiUtility.createDropdown(reqHdrList);
-
-    var reqHdrType = ['ALL Headers' , 'Specified Headers'];
-
-    this.headerTypes = ConfigUiUtility.createDropdown(reqHdrType);
-
-    var reqcaptureMode = ['complete' , 'brief'];
-
-    this.captureMode = ConfigUiUtility.createDropdown(reqcaptureMode);
-
-
-  }
+  httpRequestCustomDialog: boolean = false;
 
   ngOnInit() {
   }
+
+  openMethodDialog() {
+    this.httpRequestCustomDialog = true;
+  }
+
 
 }
