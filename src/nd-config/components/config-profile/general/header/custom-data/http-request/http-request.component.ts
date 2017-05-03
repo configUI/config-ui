dc812487d383650b99fd2c28d76cf07564361b04
Lucky <luckygoyal725@gmail.com>
2017-05-03 10:50:51 +0530

    changes for itegration-pt-detection

--- src/nd-config/components/config-profile/general/header/custom-data/http-request/http-request.component.ts
+++ src/nd-config/components/config-profile/general/header/custom-data/http-request/http-request.component.ts
@@ -195,18 +195,11 @@ export class HttpRequestComponent implements OnInit {
   /* Method for Edit HTTP Req */
   editHTTPRequest() {
     this.setEditedNewValues("Edit");
-          console.log(" ======= ==== 2 === ==-- --  ==" , this.httpRequestHdrDetail)
-
-        console.log(" ======= ==== 4 === == ==" , this.selectedHTTPReqHeader )
     this.configKeywordsService.editHTTPReqHeaderData(this.httpRequestHdrDetail, this.selectedHTTPReqHeader[0].httpReqHdrBasedId)
       .subscribe(data => {
         let index = this.getMethodBusinessIndex(data.httpReqHdrBasedId);
         this.selectedHTTPReqHeader.length = 0;
         this.selectedHTTPReqHeader.push(data);
-       console.log(" 1 == " , this.httpRequestHdrDetail)
-       console.log("2  == " , data)
-
-
         this.httpRequestHdrComponentInfo[index] = this.addReqHeaderTableData(this.httpRequestHdrDetail)[0];
         this.closeDialog();
       });
@@ -214,10 +207,6 @@ export class HttpRequestComponent implements OnInit {
     // if (this.httpRequestHdrDetail.dumpMode == 1) {
     //   if (this.rulesDataInfo.length != 0) {
     //     this.setEditedNewValues("EditSpecific");
-    //     console.log(" ======= ==== === ==-- --  ==" , this.rulesDataInfo.length)
-    //      console.log(" ======= ==== === ==-- --  ==" , this.httpRequestHdrDetail.rules)
-    //     console.log(" ======= ==== === ==-- --  ==" , this.editSpecific )
-    //     console.log(" ======= ==== === == ==" , this.selectedHTTPReqHeader[0].httpReqHdrBasedId )
     //     this.configKeywordsService.editHTTPReqHeaderRulesData(this.editSpecific, this.selectedHTTPReqHeader[0].httpReqHdrBasedId)
     //       .subscribe(data => {
     //         // let index = this.getRulesIndex(this.editSpecific.valName);
@@ -269,14 +258,12 @@ export class HttpRequestComponent implements OnInit {
  }
 
       if (opertionType == "NewAdd") {
-        console.log("new Add == ")
         this.httpRequestHdrDetail.rules = [];
         this.httpRequestHdrDetail.attrValues = [];
         this.httpRequestHdrDetail.rules[i] = { type: type, id: i, lb: this.rulesDataInfo[i].lb, rb: this.rulesDataInfo[i].rb, customValTypeName: this.rulesDataInfo[i].customValTypeName, valName: this.rulesDataInfo[i].valName };
         this.httpRequestHdrDetail.attrValues[i] = { type: type, id: i, lb: this.rulesDataInfo[i].lb, rb: this.rulesDataInfo[i].rb, customValTypeName: this.rulesDataInfo[i].customValTypeName, valName: this.rulesDataInfo[i].valName };
       }
       // if (opertionType == "EditSpecific") {
-      //   console.log("EditSpecific   == ")
       //   this.httpRequestHdrDetail.rules = [];
       //   this.httpRequestHdrDetail.attrValues = [];
       //   this.editSpecific = { type: type, id: i, lb: this.rulesDataInfo[i].lb, rb: this.rulesDataInfo[i].rb, customValTypeName: this.rulesDataInfo[i].customValTypeName, valName: this.rulesDataInfo[i].valName };
@@ -320,8 +307,6 @@ export class HttpRequestComponent implements OnInit {
     var arrTableData = [];
     let dumpModeTmp = "";
     let valueNames = "";
-
-    console.log("console log = ", data)
     if (data.rules != null) {
       for (var j = 0; j < data.rules.length; j++) {
         if (data.rules.length == 1)
@@ -353,8 +338,7 @@ export class HttpRequestComponent implements OnInit {
 
   // Method for saving rules information
   saveRules() {
-    if (this.checkRuleAlreadyExist())
-      return;
+    
     this.rulesDataInfo.push(this.rulesDataDetail);
     this.closeRulesDialog();
   }
@@ -362,6 +346,7 @@ export class HttpRequestComponent implements OnInit {
   // Method for delete rules information
   deleteRules() {
     let selectedRules = this.selectedRulesData;
+    console.log("this.selectedRulesData =  ", this.selectedRulesData);
     let arrRulesIndex = [];
     for (let index in selectedRules) {
       arrRulesIndex.push(selectedRules[index].valName);
@@ -402,11 +387,9 @@ export class HttpRequestComponent implements OnInit {
       accept: () => {
         //Get Selected Applications's AppId
         let selectedApp = this.selectedHTTPReqHeader;
-        console.log("===== === ", this.selectedHTTPReqHeader);
         let arrAppIndex = [];
         for (let index in selectedApp) {
           arrAppIndex.push(selectedApp[index].httpReqHdrBasedId);
-          console.log("===== === ", arrAppIndex);
         }
         this.configKeywordsService.deleteHTTPReqHeaderData(arrAppIndex, this.profileId)
           .subscribe(data => {
