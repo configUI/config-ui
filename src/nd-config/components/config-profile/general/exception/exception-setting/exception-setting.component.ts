2c736e51b33ca58679a18612e01bb89b1bb27fce
namanjain775 <namanjain775@gmail.com>
2017-06-23 19:35:51 +0530

    changes for bug 28848

--- src/nd-config/components/config-profile/general/exception/exception-setting/exception-setting.component.ts
+++ src/nd-config/components/config-profile/general/exception/exception-setting/exception-setting.component.ts
@@ -6,146 +6,152 @@ import { ConfigKeywordsService } from '../../../../../services/config-keywords.s
 import { ConfigUtilityService } from '../../../../../services/config-utility.service';
 import { ExceptionData } from '../../../../../containers/exception-capture-data';
 import { cloneObject } from '../../../../../utils/config-utility';
+import { ConfigurationComponent } from '../../../configuration/configuration.component';
 
 @Component({
-    selector: 'app-exception-setting',
-    templateUrl: './exception-setting.component.html',
-    styleUrls: ['./exception-setting.component.css']
+  selector: 'app-exception-setting',
+  templateUrl: './exception-setting.component.html',
+  styleUrls: ['./exception-setting.component.css']
 })
 export class ExceptionSettingComponent implements OnInit {
 
-    @Input()
-    data;
-    saveDisable: boolean;
+  @Input()
+  data;
+  saveDisable: boolean;
 
-    /**This is to send data to parent component(General Screen Component) for save keyword data */
-    @Output()
-    keywordData = new EventEmitter();
-    exception: Object;
-    /**These are those keyword which are used in current screen. */
-    keywordList: string[] = ['instrExceptions', 'enableExceptionInSeqBlob', 'enableExceptionsWithSourceAndVars'];
-    subscriptionEG: Subscription;
-    // selectedValue: string = 'unhandled';
-    enableGroupKeyword: boolean;
+  /**This is to send data to parent component(General Screen Component) for save keyword data */
+  @Output()
+  keywordData = new EventEmitter();
+  exception: Object;
+  /**These are those keyword which are used in current screen. */
+  keywordList: string[] = ['instrExceptions', 'enableExceptionInSeqBlob', 'enableExceptionsWithSourceAndVars'];
+  subscriptionEG: Subscription;
+  // selectedValue: string = 'unhandled';
+  enableGroupKeyword: boolean;
+  keywordValue: Object;
 
-    keywordValue: Object;
+  constructor(private configKeywordsService: ConfigKeywordsService, private configUtilityService: ConfigUtilityService, private store: Store<KeywordList>) {
+    this.getKeywordData();
+    configKeywordsService.toggleKeywordData();
+  }
 
-    constructor(private configKeywordsService: ConfigKeywordsService, private configUtilityService: ConfigUtilityService, private store: Store<KeywordList>) {
-        this.getKeywordData();
-        configKeywordsService.toggleKeywordData();
+  exceptionData: ExceptionData;
+  subscription: Subscription;
+  exceptionForm: boolean = true;
+
+  ngOnInit() {
+
+    if (sessionStorage.getItem('toggleDisable') == 'false') {
+      this.exceptionData.instrumentException = true;
+    }
+    else {
+      this.exceptionData.instrumentException = false;
     }
 
-    exceptionData: ExceptionData;
-    subscription: Subscription;
-    exceptionForm: boolean = true;
+  }
 
-    ngOnInit() {
-
+  getKeywordData() {
+    // let keywordData = this.configKeywordsService.keywordData;
+    if (this.configKeywordsService.keywordData != undefined) {
+      this.keywordValue = this.configKeywordsService.keywordData;
     }
-
-
-    getKeywordData() {
-        // let keywordData = this.configKeywordsService.keywordData;
-        if (this.configKeywordsService.keywordData != undefined) {
-            this.keywordValue = this.configKeywordsService.keywordData;
-        }
-        else {
-            this.subscription = this.store.select("keywordData").subscribe(data => {
-                var keywordDataVal = {}
-                this.keywordList.map(function (key) {
-                    keywordDataVal[key] = data[key];
-                })
-                this.keywordValue = keywordDataVal;
-            });
-        }
-        this.exception = {};
-
-        this.keywordList.forEach((key) => {
-            if (this.keywordValue.hasOwnProperty(key)) {
-                this.exception[key] = this.keywordValue[key];
-            }
-        });
-
-        if ((this.exception["instrExceptions"].value).includes("%20")) {
-            let arr = (this.exception["instrExceptions"].value).split("%20")
-            this.exceptionData = new ExceptionData();
-
-            if (arr[0] === "1") {
-                this.exceptionData.instrumentException = true;
-                this.exceptionData.exceptionCapturing = false;
-            }
-            else if (arr[0] === "2") {
-                this.exceptionData.instrumentException = true;
-                this.exceptionData.exceptionCapturing = true;
-            }
-            else
-                this.exceptionData.instrumentException = false;
-
-            if (arr[1] === "1")
-                this.exceptionData.exceptionTrace = true;
-            else
-                this.exceptionData.exceptionTrace = false;
-
-            this.exceptionData.exceptionType = arr[2] == 0 ? false : true;
-            if (arr.length > 3)
-                this.exceptionData.exceptionTraceDepth = arr[3];
-            else
-                this.exceptionData.exceptionTraceDepth = 999;
-        }
-        else {
-            this.exceptionData = new ExceptionData();
-            if (this.exception["instrExceptions"].value == 0) {
-                this.exceptionData.instrumentException = false;
-                this.exceptionData.exceptionCapturing = false;
-                this.exceptionData.exceptionTrace = false;
-                this.exceptionData.exceptionType = false;
-                this.exceptionData.exceptionTraceDepth = 999;
-            }
-            else if (this.exception["instrExceptions"].value == 1) {
-                this.exceptionData.instrumentException = false;
-                this.exceptionData.exceptionCapturing = false;
-                this.exceptionData.exceptionTrace = false;
-                this.exceptionData.exceptionType = false;
-                this.exceptionData.exceptionTraceDepth = 999;
-            }
-        }
+    else {
+      this.subscription = this.store.select("keywordData").subscribe(data => {
+        var keywordDataVal = {}
+        this.keywordList.map(function (key) {
+          keywordDataVal[key] = data[key];
+        })
+        this.keywordValue = keywordDataVal;
+      });
     }
+    this.exception = {};
 
-    /**Value for this keyword is
-     * 1%201%200%2012
-     * 1-  Enable instrumentException // It can be 1 or 2 [1- complete exceotion 2-L1 fp capturing]
-     * 1- enable exceptionTrace
-     * 0- false 3- true //for capture Exception type
-     * 12- Trace limit for frames //dependent on 2nd value
-     */
+    this.keywordList.forEach((key) => {
+      if (this.keywordValue.hasOwnProperty(key)) {
+        this.exception[key] = this.keywordValue[key];
+      }
+    });
 
-    // Method used to construct the value of instrException keyword.
-    instrExceptionValue(data) {
-        console.log("inside instr filter----------->");
-        var instrVal = {};
-        if (data.form._value.instrumentException === "false" || data.form._value.instrumentException === false) {
-            instrVal = "0";
-        }
-        else {
-            if (this.exceptionData.exceptionCapturing == false)
-                instrVal = "1";
+    if ((this.exception["instrExceptions"].value).includes("%20")) {
+      let arr = (this.exception["instrExceptions"].value).split("%20")
+      this.exceptionData = new ExceptionData();
 
-            if (this.exceptionData.exceptionCapturing == true)
-                instrVal = "2";
+      if (arr[0] === "1") {
+        this.exceptionData.instrumentException = true;
+        this.exceptionData.exceptionCapturing = false;
+      }
+      else if (arr[0] === "2") {
+        this.exceptionData.instrumentException = true;
+        this.exceptionData.exceptionCapturing = true;
+      }
+      else
+        this.exceptionData.instrumentException = false;
 
-            if (data.form._value.exceptionTrace === "true" || data.form._value.exceptionTrace === true)
-                instrVal = instrVal + "%201";
-            else
-                instrVal = instrVal + "%200";
+      if (arr[1] === "1")
+        this.exceptionData.exceptionTrace = true;
+      else
+        this.exceptionData.exceptionTrace = false;
 
-            if (data.form._value.exceptionType === false || data.form._value.exceptionType === "false")
-                instrVal = instrVal + "%200";
-            else
-                instrVal = instrVal + "%203"
-
-            if (data.form._value.exceptionTrace === "true" || data.form._value.exceptionTrace === true)
-                instrVal = instrVal + "%20" + data.form._value.exceptionTraceDepth;
-        }
-        return instrVal;
+      this.exceptionData.exceptionType = arr[2] == 0 ? false : true;
+      if (arr.length > 3)
+        this.exceptionData.exceptionTraceDepth = arr[3];
+      else
+        this.exceptionData.exceptionTraceDepth = 999;
     }
+    else {
+      this.exceptionData = new ExceptionData();
+      if (this.exception["instrExceptions"].value == 0) {
+        this.exceptionData.instrumentException = false;
+        this.exceptionData.exceptionCapturing = false;
+        this.exceptionData.exceptionTrace = false;
+        this.exceptionData.exceptionType = false;
+        this.exceptionData.exceptionTraceDepth = 999;
+      }
+      else if (this.exception["instrExceptions"].value == 1) {
+        this.exceptionData.instrumentException = false;
+        this.exceptionData.exceptionCapturing = false;
+        this.exceptionData.exceptionTrace = false;
+        this.exceptionData.exceptionType = false;
+        this.exceptionData.exceptionTraceDepth = 999;
+      }
+    }
+  }
+
+  /**Value for this keyword is
+   * 1%201%200%2012
+   * 1-  Enable instrumentException // It can be 1 or 2 [1- complete exceotion 2-L1 fp capturing]
+   * 1- enable exceptionTrace
+   * 0- false 3- true //for capture Exception type
+   * 12- Trace limit for frames //dependent on 2nd value
+   */
+
+  // Method used to construct the value of instrException keyword.
+  instrExceptionValue(data) {
+     console.log("inside instr filter----------->");
+    var instrVal = {};
+    if (data.form._value.instrumentException === "false" || data.form._value.instrumentException === false) {
+      instrVal = "0";
+    }
+    else {
+      if (this.exceptionData.exceptionCapturing == false)
+        instrVal = "1";
+
+      if (this.exceptionData.exceptionCapturing == true)
+        instrVal = "2";
+
+      if (data.form._value.exceptionTrace === "true" || data.form._value.exceptionTrace === true)
+        instrVal = instrVal + "%201";
+      else
+        instrVal = instrVal + "%200";
+
+      if (data.form._value.exceptionType === false || data.form._value.exceptionType === "false")
+        instrVal = instrVal + "%200";
+      else
+        instrVal = instrVal + "%203"
+
+      if (data.form._value.exceptionTrace === "true" || data.form._value.exceptionTrace === true)
+        instrVal = instrVal + "%20" + data.form._value.exceptionTraceDepth;
+    }
+    return instrVal;
+  }
 }
