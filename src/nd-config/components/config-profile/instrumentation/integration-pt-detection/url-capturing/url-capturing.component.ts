24312781fc4697ac36a961664026571c59d430fe
surbhigoell <surbhigyl99@gmail.com>
2017-08-24 13:23:03 +0530

    merge code

--- src/nd-config/components/config-profile/instrumentation/integration-pt-detection/url-capturing/url-capturing.component.ts
+++ src/nd-config/components/config-profile/instrumentation/integration-pt-detection/url-capturing/url-capturing.component.ts
@@ -134,23 +134,24 @@ export class UrlCapturingComponent implements OnInit {
   // Method used to construct the value of formatIPResourceURL keyword.
   formatIPResourceURLValue() {
     var formatIPVal = {};
-      if (this.urlCapturingData.includeParameter == false) {
+      if (this.urlCapturingData.includeParameter == false)
         formatIPVal = "0";
-      }
-      else{
-        if (this.urlCapturingData.includeParameter == true)
-           formatIPVal = "1";
+      else
+        formatIPVal = "1";
 
-        if (this.urlCapturingData.urlOffset != null)
-           formatIPVal = formatIPVal + "%20" + this.urlCapturingData.urlOffset;
-        else
-           formatIPVal = formatIPVal + "%200";
+      if (this.urlCapturingData.urlOffset != null)
+        formatIPVal = formatIPVal + "%20" + this.urlCapturingData.urlOffset;
+      else
+        formatIPVal = formatIPVal + "%200";
 
-        if (this.urlCapturingData.maxChar != null)
-           formatIPVal = formatIPVal + "%20" + this.urlCapturingData.maxChar;
-        else
-           formatIPVal = formatIPVal + "%20256"
+      if (this.urlCapturingData.maxChar != null)
+        formatIPVal = formatIPVal + "%20" + this.urlCapturingData.maxChar;
+      else
+        formatIPVal = formatIPVal + "%20256"
+      if(formatIPVal == "0%200%20256"){
+        return 0;
       }
+      else
         return formatIPVal;
      }
 
--- src/nd-config/components/config-profile/instrumentation/service-entry-point/service-entry-point.component.ts
+++ src/nd-config/components/config-profile/instrumentation/service-entry-point/service-entry-point.component.ts
@@ -38,10 +38,14 @@ export class ServiceEntryPointComponent implements OnInit {
   /**It is used as flag to open or close dialog */
   displayNewService: boolean = false;
 
+  addEditServiceEntryDialog:boolean = false;
+  isNewServiceEntryPoint:boolean;
+
   constructor(private configKeywordsService: ConfigKeywordsService, private configUtilityService: ConfigUtilityService,private confirmationService: ConfirmationService) { }
 
   ngOnInit() {
     this.loadServiceEntryPoint();
+    this.loadEntryPointTypeList();
     this.saveDisable = this.profileId == 1 ? true : false;
   }
   /**It loads service entry data  */
@@ -52,39 +56,70 @@ export class ServiceEntryPointComponent implements OnInit {
 
   /**This method is called when we click on Add button to open dialog  */
   openServiceEntryDialog() {
-    this.loadEntryPointTypeList();
     this.serviceEntryPointDetail = new ServiceEntryPoint();
-    this.displayNewService = true;
+    this.isNewServiceEntryPoint = true;
+    this.addEditServiceEntryDialog = true;
   }
   /**It adds data in dropdown of Entry type service */
   loadEntryPointTypeList() {
     //EntryPointType contains some values then return. no need to get data from server.
     if (this.entryPointType)
       return;
-
     this.entryPointType = [];
-
+    let arr = [];
     this.configKeywordsService.getEntryPointTypeList()
       .subscribe(entryPointTypeList => {
         for (let i = 0; i < entryPointTypeList.length; i++) {
-          this.entryPointType.push({ label: entryPointTypeList[i].entryTypeName, value: entryPointTypeList[i].entryTypeId });
+          arr.push(entryPointTypeList[i].entryTypeName);
+        }
+        arr.sort();
+        for (let i = 0; i < arr.length; i++){
+          for (let j = 0; j < entryPointTypeList.length; j++){
+              if(arr[i] == entryPointTypeList[j].entryTypeName){
+                this.entryPointType.push({ label: arr[i], value: entryPointTypeList[j].entryTypeId });
+              }
+          }
         }
       });
   }
 
+
   /**It stores the dialog data back to the backend */
   saveServiceEntryPointService(): void {
+    //When add new Service Entry Point
+    if (this.isNewServiceEntryPoint) {
+      //Check for service entry point name already exist or not
+      if (!this.checkServiceEntryPointNameAlreadyExist()) {
+        this.saveServiceEntry();
+        this.loadEntryPointTypeList();
+        return;
+      }
+    }
+    //When add edit service entry point
+    else {
+      if (this.selectedServiceEntryData[0].name != this.serviceEntryPointDetail.name) {
+        if (this.checkServiceEntryPointNameAlreadyExist())
+          return;
+      }
+      this.editServiceEntry();
+      this.loadEntryPointTypeList();
+    }
+  }
+
+  saveServiceEntry(): void {
     if (this.serviceEntryPointDetail.desc != null) {
       if (this.serviceEntryPointDetail.desc.length > 500) {
         this.configUtilityService.errorMessage(descMsg);
         return;
       }
     }
+
     for (let i = 0; i < this.entryPointType.length; i++) {
       if (this.serviceEntryPointDetail.entryTypeId == this.entryPointType[i].value) {
         this.serviceEntryPointDetail.entryType = this.entryPointType[i].label;
       }
     }
+
     this.serviceEntryPointDetail.fqm = this.serviceEntryPointDetail.fqm.trim();
     this.configKeywordsService.addServiceEntryPointData(this.serviceEntryPointDetail, this.profileId)
       .subscribe(data => {
@@ -93,7 +128,7 @@ export class ServiceEntryPointComponent implements OnInit {
         this.serviceEntryData = ImmutableArray.push(this.serviceEntryData, data);
         this.configUtilityService.successMessage(Messages);
       });
-    this.displayNewService = false;
+    this.addEditServiceEntryDialog = false;
   }
 
   /**Used to enabled/Disabled Service Entry Points */
@@ -144,7 +179,7 @@ deleteServiceEntryPoint():void{
         for (let index in selectedApp) {
           arrAppIndex.push(selectedApp[index].id);
         }
-      
+
         this.configKeywordsService.deleteServiceEntryData(arrAppIndex, this.profileId)
           .subscribe(data => {
             this.deleteServiceEntryData(arrAppIndex);
@@ -155,7 +190,7 @@ deleteServiceEntryPoint():void{
       reject: () => {
       }
     });
-  }  
+  }
   /**This method is used to delete  from Data Table */
   deleteServiceEntryData(arrAppIndex) {
     let rowIndex: number[] = [];
@@ -173,6 +208,89 @@ deleteServiceEntryPoint():void{
         return i;
       }
     }
+    return -1;
+  }
+
+  /**For showing Edit Service Entry Point dialog */
+  openEditServiceEntryDialog(): void {
+
+    if (!this.selectedServiceEntryData || this.selectedServiceEntryData.length < 1) {
+      this.configUtilityService.errorMessage("Select a row to edit");
+      return;
+    }
+    else if (this.selectedServiceEntryData.length > 1) {
+      this.configUtilityService.errorMessage("Select only one row to edit");
+      return;
+    }
+
+    /**
+      * Check if selected row(s) contain pre-defined Service Entry Point(s)
+      */
+      let selectedEntry = this.selectedServiceEntryData;
+        let arrAppIndex = [];
+         for (let index in selectedEntry) {
+          arrAppIndex.push(selectedEntry[index].isCustomEntry);
+         }
+        let i:number;
+        for( i=0;i<11;i++){
+          if(arrAppIndex[i]==false){
+            this.configUtilityService.errorMessage("Predefined Service Entry Point can't be edited");
+             return;
+            }
+          }
+
+    this.serviceEntryPointDetail = new ServiceEntryPoint();
+    this.isNewServiceEntryPoint = false;
+    this.addEditServiceEntryDialog = true;
+    this.serviceEntryPointDetail = Object.assign({}, this.selectedServiceEntryData[0]);
+  }
+
+  /**This method is used to validate the name of error Detection is already exists. */
+  checkServiceEntryPointNameAlreadyExist(): boolean {
+    for (let i = 0; i < this.serviceEntryData.length; i++) {
+      if (this.serviceEntryData[i].name == this.serviceEntryPointDetail.name) {
+        this.configUtilityService.errorMessage("Service entry point name already exist");
+        return true;
+      }
+    }
+  }
+
+  editServiceEntry(): void {
+
+    if (this.serviceEntryPointDetail.desc != null) {
+      if (this.serviceEntryPointDetail.desc.length > 500) {
+        this.configUtilityService.errorMessage(descMsg);
+        return;
+      }
+    }
+
+    for (let i = 0; i < this.entryPointType.length; i++) {
+      if (this.serviceEntryPointDetail.entryTypeId == this.entryPointType[i].value) {
+        this.serviceEntryPointDetail.entryType = this.entryPointType[i].label;
+      }
+    }
+
+    this.serviceEntryPointDetail.fqm = this.serviceEntryPointDetail.fqm.trim();
+    this.configKeywordsService.editServiceEntryPointData(this.serviceEntryPointDetail, this.profileId)
+      .subscribe(data => {
+        let index = this.getServiceEntryPoint();
+        this.serviceEntryData = ImmutableArray.replace(this.serviceEntryData, data,index);
+        this.serviceEntryPointDetail.entryType = data.entryType
+        this.configUtilityService.successMessage(Messages);
+      });
+    this.addEditServiceEntryDialog = false;
+    this.selectedServiceEntryData=[];
+  }
+
+  getServiceEntryPoint(): number {
+    if(this.serviceEntryPointDetail){
+      let ID = this.serviceEntryPointDetail.id;
+    for (let i = 0; i < this.serviceEntryData.length; i++) {
+      if (this.serviceEntryData[i].id == ID) {
+        return i;
+      }
+    }
+  }
     return -1;
   }
 
