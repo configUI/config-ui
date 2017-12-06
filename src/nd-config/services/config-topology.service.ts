82a3034745b5e9aac518d4deed90bf90cbcbc263
surbhigoell <surbhigyl99@gmail.com>
2017-12-06 12:35:51 +0530

    Error msg

--- src/nd-config/services/config-topology.service.ts
+++ src/nd-config/services/config-topology.service.ts
@@ -15,22 +15,22 @@ export class ConfigTopologyService {
 
   private _tableData: Object;
 
-  
-	public get tableData(): Object {
-		return this._tableData;
-	}
 
-	public set tableData(value: Object) {
-		this._tableData = value;
-	}
+  public get tableData(): Object {
+    return this._tableData;
+  }
 
-	public get topoTreeData(): Object {
-		return this._topoTreeData;
-	}
+  public set tableData(value: Object) {
+    this._tableData = value;
+  }
 
-	public set topoTreeData(value: Object) {
-		this._topoTreeData = value;
-	}
+  public get topoTreeData(): Object {
+    return this._topoTreeData;
+  }
+
+  public set topoTreeData(value: Object) {
+    this._topoTreeData = value;
+  }
 
 
 
@@ -57,93 +57,93 @@ export class ConfigTopologyService {
     return this._restApi.getDataByPostReq(`${URL.FETCH_INSTANCE_TABLE_URL}/${serverId}`, entity);
   }
 
-  getTopologyTreeDetail(dcId: number) :Observable<TreeNode[]>{
-     return  this._restApi.getDataByGetReq(`${URL.FETCH_TOPO_TREE_URL}/${dcId}`);
+  getTopologyTreeDetail(dcId: number): Observable<TreeNode[]> {
+    return this._restApi.getDataByGetReq(`${URL.FETCH_TOPO_TREE_URL}/${dcId}`);
   }
 
-  getTierTreeDetail(tierId:number,profileId:number) : Observable<TreeNode[]>{
+  getTierTreeDetail(tierId: number, profileId: number): Observable<TreeNode[]> {
     return this._restApi.getDataByGetReq(`${URL.FETCH_TIER_TREE_URL}/${tierId}/${profileId}`);
   }
 
-  getServerTreeDetail(serverId:number,profileId:number) :Observable<TreeNode[]>{
-     return this._restApi.getDataByGetReq(`${URL.FETCH_SERVER_TREE_URL}/${serverId}/${profileId}`);
+  getServerTreeDetail(serverId: number, profileId: number): Observable<TreeNode[]> {
+    return this._restApi.getDataByGetReq(`${URL.FETCH_SERVER_TREE_URL}/${serverId}/${profileId}`);
   }
 
 
-  getTierTableData(tierId:number,profileId:number){
+  getTierTableData(tierId: number, profileId: number) {
     this._restApi.getDataByGetReq(`${URL.FETCH_TIER_TABLE_URL}/${tierId}/{profileId}`).subscribe(data => this._tableData = data)
   }
 
-/* Changing Profile To Topology */
-  updateAttachedProfTopo(data) :Observable<Object>{
+  /* Changing Profile To Topology */
+  updateAttachedProfTopo(data): Observable<Object> {
     return this._restApi.getDataByGetReq(`${URL.ATTACH_PROFTO_TOPO}/${data.dcTopoId}/${data.profileId}`);
   }
 
-/* Changing Profile to Tier */
-  updateAttachedProfTier(data){
+  /* Changing Profile to Tier */
+  updateAttachedProfTier(data) {
     return this._restApi.getDataByGetReq(`${URL.ATTACH_PROFTO_TIER}/${data.tierId}/${data.profileId}`);
   }
   /* Changing Profile to Server */
-  updateAttachedProfServer(data){
+  updateAttachedProfServer(data) {
     return this._restApi.getDataByGetReq(`${URL.ATTACH_PROFTO_SERVER}/${data.serverId}/${data.profileId}`);
   }
 
   /* Changing Profile to Instance */
-  updateAttachedProfInstance(data){
+  updateAttachedProfInstance(data) {
     return this._restApi.getDataByGetReq(`${URL.ATTACH_PROFTO_INSTANCE}/${data.instanceId}/${data.profileId}`);
   }
 
   /**For disable Profile at instance level */
-  disableProfInstance(instanceId, flag){
+  disableProfInstance(instanceId, flag) {
     return this._restApi.getDataByPutReq(`${URL.TOGGLED_INSTANCE_STATE}/${instanceId}/${flag}`);
   }
 
-  getTopologyStructure(topoId){
+  getTopologyStructure(topoId) {
     return this._restApi.getDataByGetReq(`${URL.TOPOLOGY_TREE_STRUCTURE}/${topoId}`);
   }
 
-   getTopologyStructureTableData(topoId){
-     return this._restApi.getDataByGetReq(`${URL.TOPOLOGY_STRUCT_TABLEDATA}/${topoId}`);
-   }
+  getTopologyStructureTableData(topoId) {
+    return this._restApi.getDataByGetReq(`${URL.TOPOLOGY_STRUCT_TABLEDATA}/${topoId}`);
+  }
 
-  getLazyFiles(){
+  getLazyFiles() {
     let data = {
-    "data": 
-    [
-     {"label": "Expenses.doc", "icon": "fa-file-word-o", "data": "Expenses Document"}, {"label": "Resume.doc", "icon": "fa-file-word-o", "data": "Resume Document"}
-    ]
-};
+      "data":
+      [
+        { "label": "Expenses.doc", "icon": "fa-file-word-o", "data": "Expenses Document" }, { "label": "Resume.doc", "icon": "fa-file-word-o", "data": "Resume Document" }
+      ]
+    };
     return data.data;
   }
 
-/**To apply auto-instrumentation  */
-applyAutoInstr(data): Observable<AutoIntrDTO> {
-  return this._restApi.getDataByPostReq(`${URL.APPLY_AUTO_INSTR}`, data);
-}
+  /**To apply auto-instrumentation  */
+  applyAutoInstr(data): Observable<AutoIntrDTO> {
+    return this._restApi.getDataByPostReq(`${URL.APPLY_AUTO_INSTR}`, data);
+  }
 
-/**To apply auto-instrumentation  */
-stopAutoInstr(instanceName): Observable<AutoIntrDTO[]>{
-  return this._restApi.getDataByPostReq(`${URL.STOP_AUTO_INSTR}`, instanceName);
-}
+  /**To apply auto-instrumentation  */
+  stopAutoInstr(instanceName): Observable<AutoIntrDTO[]> {
+    return this._restApi.getDataByPostReq(`${URL.STOP_AUTO_INSTR}`, instanceName);
+  }
 
-/**To get auto-instrumentation settings data to display in dialog */
-getAutoInstr(appName, instanceName, sessionName){
-  return this._restApi.getDataByPostReqWithNoJSON(`${URL.GET_AUTO_INSTR_DATA}/${appName}`, instanceName+"#"+sessionName );
-}
+  /**To get auto-instrumentation settings data to display in dialog */
+  getAutoInstr(appName, instanceName, sessionName) {
+    return this._restApi.getDataByPostReqWithNoJSON(`${URL.GET_AUTO_INSTR_DATA}/${appName}`, instanceName + "#" + sessionName);
+  }
 
-getServerDisplayName(instanceId: number): Observable<String> {
-  return this._restApi.getDataByPostReqWithNoJSON(`${URL.GET_SERVER_DIS_NAME}/${instanceId}`);
-}
+  getServerDisplayName(instanceId: number): Observable<String> {
+    return this._restApi.getDataByPostReqWithNoJSON(`${URL.GET_SERVER_DIS_NAME}/${instanceId}`);
+  }
 
 
   //Send RTC on AI(running mode)
-  sendRTCAutoInstr(url, data, autoInstrDto, callback){
+  sendRTCAutoInstr(url, data, autoInstrDto, callback) {
     let success = ""
     this._restApi.getDataByPostReq(url, data).subscribe(data => {
 
       //When result=OK
       if (data[0].includes("result=OK") || data[0].includes("result=Ok")) {
-        
+
         //Request to save in database if NDC sends OK
         this.applyAutoInstr(autoInstrDto).subscribe(data => {
           this.configUtilityService.infoMessage("Auto Instrumentation started");
@@ -151,20 +151,24 @@ getServerDisplayName(instanceId: number): Observable<String> {
           callback(success)
         })
       }
-      
+
       //When result=Error, then no RTC applied
       else {
         //Getting error if any and showing as toaster message
         let err = data[0].substring(data[0].lastIndexOf(":") + 1)
-        this.configUtilityService.errorMessage("Could not start: " + err);
-         success = "fail";
-         callback(success)
+        if (err.includes("action=modify;result=Error;"))
+          this.configUtilityService.errorMessage("Could not start: This instance is not connected");
+        else
+          this.configUtilityService.errorMessage("Could not start: " + err);
+
+        success = "fail";
+        callback(success)
       }
     });
   }
 
   //Send RTC on AI(Stop mode)
-  sendRTCTostopAutoInstr(url, data, instanceName,sessionName, callback){
+  sendRTCTostopAutoInstr(url, data, instanceName, sessionName, callback) {
     this._restApi.getDataByPostReq(url, data).subscribe(data => {
 
       //When result=OK
@@ -178,7 +182,7 @@ getServerDisplayName(instanceId: number): Observable<String> {
           callback(data)
         })
       }
-      
+
       //When result=Error, then no RTC applied
       else {
         //Getting error if any and showing as toaster message
@@ -192,33 +196,33 @@ getServerDisplayName(instanceId: number): Observable<String> {
   //Get Auto Instrumentation Data to show  in table
   getAIData(): Observable<AutoIntrDTO[]> {
     return this._restApi.getDataByGetReq(`${URL.GET_AUTO_INSTR_TABLE_DATA}`);
-  }  
+  }
 
-    //Update AI details when duration is completed
-    updateAIDetails(): Observable<AutoIntrDTO[]> {
-      return this._restApi.getDataByGetReq(`${URL.UPDATE_AI_DETAILS}`);
-    }  
-  
+  //Update AI details when duration is completed
+  updateAIDetails(): Observable<AutoIntrDTO[]> {
+    return this._restApi.getDataByGetReq(`${URL.UPDATE_AI_DETAILS}`);
+  }
+
 
   //Get Auto Instrumentation Data to show  in table
-  getSessionFileExistOrNot(sessionFileName){
+  getSessionFileExistOrNot(sessionFileName) {
     return this._restApi.getDataByPostReqWithNoJSON(`${URL.FILE_EXIST_OR_NOT}`, sessionFileName);
-  }  
+  }
 
   //Get status of the selected AI
-    getAIStatus(instance){
-      return this._restApi.getDataByPostReqWithNoJSON(`${URL.GET_AI_STATUS}`, instance);
-    }  
+  getAIStatus(instance) {
+    return this._restApi.getDataByPostReqWithNoJSON(`${URL.GET_AI_STATUS}`, instance);
+  }
 
-    //Download File after AI
-    downloadFile(data){
-      return this._restApi.getDataByPostReqWithNoJSON(`${URL.DOWNLOAD_FILE}`, data);
-    }  
+  //Download File after AI
+  downloadFile(data) {
+    return this._restApi.getDataByPostReqWithNoJSON(`${URL.DOWNLOAD_FILE}`, data);
+  }
 
-    //Update AI enable in Instance table
-    updateAIEnable(instanceId, flag){
-      return this._restApi.getDataByPostReqWithNoJSON(`${URL.UPDATE_AI_ENABLE}/${instanceId}`, flag);
-    }
+  //Update AI enable in Instance table
+  updateAIEnable(instanceId, flag) {
+    return this._restApi.getDataByPostReqWithNoJSON(`${URL.UPDATE_AI_ENABLE}/${instanceId}`, flag);
+  }
 
 
 }
