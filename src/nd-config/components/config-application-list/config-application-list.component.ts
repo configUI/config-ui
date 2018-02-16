d19ca79549718211cbe7363a73dce40e92923fbf
surbhigoell <surbhigyl99@gmail.com>
2018-02-16 13:32:05 +0530

    adding topolgy on home screen

--- src/nd-config/components/config-application-list/config-application-list.component.ts
+++ src/nd-config/components/config-application-list/config-application-list.component.ts
@@ -185,11 +185,11 @@ export class ConfigApplicationListComponent implements OnInit {
     }
     let arr = []
     arr.push(this.applicationDetail.topoName)
-    this.configApplicationService.addTopoDetails(arr).subscribe(data => {
-      for(let i=0;i<data.length;i++){
-        if(data[i].name == this.applicationDetail.topoName)
-          this.applicationDetail.topoId = data[i].id
-      }
+    // this.configApplicationService.addTopoDetails(arr).subscribe(data => {
+    //   for(let i=0;i<data.length;i++){
+    //     if(data[i].name == this.applicationDetail.topoName)
+    //       this.applicationDetail.topoId = data[i].id
+    //   }
       this.configApplicationService.addApplicationData(this.applicationDetail)
         .subscribe(data => {
           //Insert data in main table after inserting application in DB
@@ -200,7 +200,7 @@ export class ConfigApplicationListComponent implements OnInit {
           this.configUtilityService.successMessage(Messages);
           this.loadApplicationData();
         });
-    });
+    // });
     this.closeDialog();
   }
 
@@ -214,11 +214,11 @@ export class ConfigApplicationListComponent implements OnInit {
     }
     let arr = []
     arr.push(this.applicationDetail.topoName)
-    this.configApplicationService.addTopoDetails(arr).subscribe(data => {
-      for(let i=0;i<data.length;i++){
-        if(data[i].name == this.applicationDetail.topoName)
-          this.applicationDetail.topoId = data[i].id
-      }
+    // this.configApplicationService.addTopoDetails(arr).subscribe(data => {
+    //   for(let i=0;i<data.length;i++){
+    //     if(data[i].name == this.applicationDetail.topoName)
+    //       this.applicationDetail.topoId = data[i].id
+    //   }
     this.configApplicationService.editApplicationData(this.applicationDetail)
       .subscribe(data => {
         let index = this.getAppIndex(this.applicationDetail.appId);
@@ -231,7 +231,7 @@ export class ConfigApplicationListComponent implements OnInit {
         this.loadApplicationData();
         this.selectedApplicationData.length = 0;
       });
-    });
+    // });
     this.closeDialog();
   }
 
