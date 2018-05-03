f73981dbfa8908ae613a7e16217830f92ffcd85f
Lucky <luckygoyal725@gmail.com>
2018-05-03 17:15:23 +0530

    changes for Audit log

--- src/nd-config/services/config-rest-api.service.ts
+++ src/nd-config/services/config-rest-api.service.ts
@@ -8,8 +8,11 @@ import './config-rxjs-import';
 
 import {ConfigUtilityService} from './config-utility.service';
 
+import * as URL from '../constants/config-url-constant';
+
 @Injectable()
 export class ConfigRestApiService {
+
 
   constructor(private _navService: CavTopPanelNavigationService, private _productConfig: CavConfigService, private http: Http, private configUtilityService: ConfigUtilityService) { }
 
@@ -17,12 +20,26 @@ export class ConfigRestApiService {
  // productKey = this._productConfig.$productKey;
   // Fetch data
 
+  /**
+   * This method is used for set user name in audit log 
+   * @param url 
+   */
+  setUserNameForAuditLog(url: string):void
+  { 
+    this.http.get(url).subscribe((data) => {
+      console.log('Data: ', data);
+    })
+  }
+
   getDataByGetReq(url: string): Observable<any> {
     // if(url.indexOf("?") > 0)
     //  url = this._productConfig.getINSPrefix () +  this._navService.getDCNameForScreen('ndConfig') + url + "&productKey=" + this.productKey;
     // else
     //  url = this._productConfig.getINSPrefix () +  this._navService.getDCNameForScreen('ndConfig') + url + "?productKey=" + this.productKey;
     
+   var userNameUrl = `${URL.LOGGED_USER_NAME}/${sessionStorage.getItem("sesLoginName")}`;
+   this.setUserNameForAuditLog(userNameUrl);
+
     this.configUtilityService.progressBarEmit({flag: true, color: 'primary'});
     // ...using get request
     return this.http.get(url)
@@ -40,10 +57,10 @@ export class ConfigRestApiService {
   //    url = this._productConfig.getINSPrefix () +  this._navService.getDCNameForScreen('ndConfig') + url + "&productKey=" + this.productKey;
   //   else
   //    url = this._productConfig.getINSPrefix () +  this._navService.getDCNameForScreen('ndConfig') + url + "?productKey=" + this.productKey;
-  
+
  // this.configUtilityService.progressBarEmit({flag: true, color: 'primary'});
     // ...using get request
-    return this.http.get(url)
+      return this.http.get(url)
       // ...and calling .json() on the response to return data
       .map((res: Response) => {
         //this.configUtilityService.progressBarEmit({flag: false, color: 'primary'});
@@ -60,6 +77,9 @@ export class ConfigRestApiService {
   //   else
   //    url = this._productConfig.getINSPrefix () +  this._navService.getDCNameForScreen('ndConfig') + url + "?productKey=" + this.productKey;   
  
+  var userNameUrl = `${URL.LOGGED_USER_NAME}/${sessionStorage.getItem("sesLoginName")}`;
+  this.setUserNameForAuditLog(userNameUrl);
+
    this.configUtilityService.progressBarEmit({flag: true, color: 'primary'});
     // ...using get request
     return this.http.get(url)
@@ -77,6 +97,9 @@ export class ConfigRestApiService {
   //   else
   //    url = this._productConfig.getINSPrefix () +  this._navService.getDCNameForScreen('ndConfig') + url + "?productKey=" + this.productKey;   
  
+  var userNameUrl = `${URL.LOGGED_USER_NAME}/${sessionStorage.getItem("sesLoginName")}`;
+  this.setUserNameForAuditLog(userNameUrl);
+
    this.configUtilityService.progressBarEmit({flag: true, color: 'primary'});
     if(body == undefined)
       body = {};
@@ -95,12 +118,17 @@ export class ConfigRestApiService {
       .catch((error: any) => { this.configUtilityService.progressBarEmit({flag: true, color: 'warn'}); return Observable.throw(error.json().error || 'Server Error' )});
   }
 
+
+  
   getDataByPutReq(url: string, body?: Object): Observable<any> {
   //  if(url.indexOf("?") > 0)
   //    url = this._productConfig.getINSPrefix () +  this._navService.getDCNameForScreen('ndConfig') + url + "&productKey=" + this.productKey;
   //   else
   //    url = this._productConfig.getINSPrefix () +  this._navService.getDCNameForScreen('ndConfig') + url + "?productKey=" + this.productKey;
-   
+
+  var userNameUrl = `${URL.LOGGED_USER_NAME}/${sessionStorage.getItem("sesLoginName")}`;
+  this.setUserNameForAuditLog(userNameUrl);
+
     let headers = new Headers({ 'Content-type': 'application/json' });// ... Set content type to JSON
     let options = new RequestOptions({ headers: headers });// Create a request option
     
@@ -119,6 +147,9 @@ export class ConfigRestApiService {
   //   else
   //    url = this._productConfig.getINSPrefix () +  this._navService.getDCNameForScreen('ndAgent') + url + "?productKey=" + this.productKey;  
   
+  var userNameUrl = `${URL.LOGGED_USER_NAME}/${sessionStorage.getItem("sesLoginName")}`;
+  this.setUserNameForAuditLog(userNameUrl);
+
   this.configUtilityService.progressBarEmit({flag: true, color: 'primary'});
     // ...using get request
     return this.http.get(url)
@@ -137,6 +168,10 @@ export class ConfigRestApiService {
   //    url = this._productConfig.getURLByActiveDC() + url + "&productKey=" + this.productKey;
   //   else
   //    url = this._productConfig.getURLByActiveDC() + url + "?productKey=" + this.productKey;  
+
+  var userNameUrl = `${URL.LOGGED_USER_NAME}/${sessionStorage.getItem("sesLoginName")}`;
+  this.setUserNameForAuditLog(userNameUrl);
+
   this.configUtilityService.progressBarEmit({flag: true, color: 'primary'});
     if(body == undefined)
       body = {};
@@ -159,7 +194,10 @@ export class ConfigRestApiService {
     //  url = this._productConfig.getINSPrefix () +  this._navService.getDCNameForScreen('ndConfig') + url + "&productKey=" + this.productKey;
     // else
     //  url = this._productConfig.getINSPrefix () +  this._navService.getDCNameForScreen('ndConfig') + url + "?productKey=" + this.productKey;  
-  this.configUtilityService.progressBarEmit({flag: true, color: 'primary'});
+    var userNameUrl = `${URL.LOGGED_USER_NAME}/${sessionStorage.getItem("sesLoginName")}`;
+    this.setUserNameForAuditLog(userNameUrl);
+
+    this.configUtilityService.progressBarEmit({flag: true, color: 'primary'});
     if(body == undefined)
       body = {};
     let headers = new Headers({ 'Content-type': 'application/json' });// ... Set content type to JSON
