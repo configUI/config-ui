2dbfeb0db17627e17a35d033e6b434ad4e1fb78c
Lucky <luckygoyal725@gmail.com>
2018-10-31 11:34:25 +0530

    rtc CHANGES

--- src/nd-config/services/config-keywords.service.ts
+++ src/nd-config/services/config-keywords.service.ts
@@ -478,7 +478,11 @@ export class ConfigKeywordsService {
           rtcMsg = [];
           rtcErrMsg = [];
         }
-
+        else if (data[0].includes("NoChangesInConfiguration")) {
+          this.configUtilityService.errorMessage(" Changes in configuration for Runtime changes.");
+          rtcMsg = [];
+          rtcErrMsg = [];
+        }
         //When result=Error, then no RTC applied
         else {
           this.configUtilityService.errorMessage(" No runtime changes applied");
